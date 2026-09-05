a32c770a603bee1ca571d64ade3b17e1d34ea2e0
EllfredTheHuman <ellfred189@gmail.com>
2026-09-05 16:21:57 +1000

    Refactor Firebase authentication and Firestore imports

--- firebase.js
+++ firebase.js
@@ -1,32 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+
 import {
-    getAuth,
-    signInAnonymously
+getAuth,
+signInAnonymously
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
 import {
-    getFirestore
+getFirestore
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCP35K-2Urj4axmtcVfAe955x6lAeDK8YE",
-    authDomain: "most-common-man.firebaseapp.com",
-    projectId: "most-common-man",
-    storageBucket: "most-common-man.firebasestorage.app",
-    messagingSenderId: "1011338202971",
-    appId: "1:1011338202971:web:98766ee07c2e380c60147e"
+apiKey: "AIzaSyCP35K-2Urj4axmtcVfAe955x6lAeDK8YE",
+authDomain: "most-common-man.firebaseapp.com",
+projectId: "most-common-man",
+storageBucket: "most-common-man.firebasestorage.app",
+messagingSenderId: "1011338202971",
+appId: "1:1011338202971:web:98766ee07c2e380c60147e"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
 
 export async function loginAnonymously() {
-    if (auth.currentUser) {
-        return auth.currentUser;
-    }
 
-    const result = await signInAnonymously(auth);
-    return result.user;
+```
+if (auth.currentUser) {
+    return auth.currentUser;
+}
+
+const result = await signInAnonymously(auth);
+
+return result.user;
+```
+
 }
