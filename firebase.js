27c0aa501b312ab2311b2ebcd1255b5fa1d037bf
EllfredTheHuman <ellfred189@gmail.com>
2026-09-05 15:19:57 +1000

    Implement anonymous login in Firebase auth
    Added anonymous login functionality to Firebase authentication.

--- firebase.js
+++ firebase.js
@@ -1,9 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
+import {
+    getAuth,
+    signInAnonymously
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCP35K2-Urj4axmtcVfAe955x6lAeDK8YE",
+    apiKey: "AIzaSyCP35K-2Urj4axmtcVfAe955x6lAeDK8YE",
     authDomain: "most-common-man.firebaseapp.com",
     projectId: "most-common-man",
     storageBucket: "most-common-man.firebasestorage.app",
@@ -15,3 +21,12 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+export async function loginAnonymously() {
+    if (auth.currentUser) {
+        return auth.currentUser;
+    }
+
+    const result = await signInAnonymously(auth);
+    return result.user;
+}
