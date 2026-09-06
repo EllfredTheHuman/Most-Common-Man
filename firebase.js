b6e995f216626c0ef4d783baefcecb7277c7556a
EllfredTheHuman <ellfred189@gmail.com>
2026-09-06 16:15:23 +1000

    Refactor Firebase initialization and configuration
    Updated Firebase imports to use version 12.1.0 and modified firebaseConfig with placeholder values.

--- firebase.js
+++ firebase.js
@@ -1,41 +1,29 @@
-import {
-    initializeApp
-} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
 import {
     getAuth,
     signInAnonymously
-} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
-
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 import {
     getFirestore
-} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
-
+} from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCP35K2-Urj4axmtcVfAe955x6lAeDK8YE",
-    authDomain: "most-common-man.firebaseapp.com",
-    projectId: "most-common-man",
-    storageBucket: "most-common-man.firebasestorage.app",
-    messagingSenderId: "1011338202971",
-    appId: "1:1011338202971:web:98766ee07c2e380c60147e"
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_PROJECT.firebaseapp.com",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_PROJECT.firebasestorage.app",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
 };
-
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-
-export async function loginAnonymously() {
-
-    if (auth.currentUser) {
-        return auth.currentUser;
-    }
-
-    const result =
-        await signInAnonymously(auth);
-
-    return result.user;
-}
+export {
+    app,
+    auth,
+    db,
+    signInAnonymously
+};
